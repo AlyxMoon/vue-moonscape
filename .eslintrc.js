51db20a5eb30de5e771f18dccddc330d94e858ec
AlyxMoon <allisterkmoon@gmail.com>
2021-02-22 16:11:21 -0800

    get some actual framework set up I don't know yet how this is going to turn into a package but that's a future-me problem!

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-strongly-recommended',
+    '@vue/standard',
+    '@vue/typescript/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  plugins: [
+    'vue',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+
+    // ignore eslint rules that also exist in typescript
+    'no-unused-vars': 'off',
+
+    // personal preferences
+    'comma-dangle': ['error', 'always-multiline'],
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+}
---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  Hello World
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+})
+</script>
+
+<style lang="scss">
+</style>
